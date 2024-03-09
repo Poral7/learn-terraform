f57b401e000ed6fab0b3751f5cbc3b68b8732c56
sunil <poralsunil5@gmail.com>
2024-03-09 08:06:51 +0530

    jenkins-file

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -1,6 +1,59 @@
 variable "sample" {
   default = 10
 }
+
 output "sample" {
   value = var.sample
 }
+
+# string data type
+
+variable "sample1" {
+  default = "Hello World"
+}
+
+# number data type
+
+variable "sample2" {
+  default = 100
+}
+
+# boolean data type
+
+variable "sample3" {
+  default = true
+}
+
+# default variable type
+
+variable "sample4" {
+  default = 100
+}
+output "sample4" {
+  value = "var.sample4"
+}
+
+#list variable type
+variable "sample5"{
+  default = [
+    100,
+    "abc",
+    "xyz"
+  ]
+}
+output "sample5" {
+  value = "var.sample5[1]"
+}
+#map variable type
+variable "sample6" {
+  default = {
+    number = 100
+    string = "xyz"
+    boolean = false
+  }
+}
+
+output "sample6" {
+  value = var.sample6["number"]
+}
+
