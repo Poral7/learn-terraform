288947f275003b2b94ff8ce5d5dd38889d45c9c1
sunil <poralsunil5@gmail.com>
2024-03-09 05:47:53 +0530

    jenkins-file

--- 02-data-sources/data.tf
+++ 02-data-sources/data.tf
@@ -7,3 +7,14 @@ data "aws_ami" "example" {
 output "ami"{
   value = data.aws_ami.example
 }
+
+
+data "aws_instance" "foo" {
+  instance_id = "i-0aa362f1fc9d07698"
+}
+
+output "instance" {
+  value = "data.aws_instance.foo.public_ip"
+}
+
+
