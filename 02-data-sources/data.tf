3abcce07e24eebd5be777f043cc719bd5c3a3075
sunil <poralsunil5@gmail.com>
2024-03-26 10:51:41 +0530

    jenkins-file

--- 02-data-sources/data.tf
+++ 02-data-sources/data.tf
@@ -3,7 +3,6 @@ data "aws_ami" "example" {
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
 }
-
 output "ami"{
   value = data.aws_ami.example
 }
@@ -12,7 +11,6 @@ output "ami"{
 data "aws_instance" "foo" {
   instance_id = "i-0aa362f1fc9d07698"
 }
-
 output "instance" {
   value = data.aws_instance.foo.public_ip
 }
