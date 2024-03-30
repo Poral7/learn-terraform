115420c834a135c2208d8ddbb4c377b1abf0b262
sunil <poralsunil5@gmail.com>
2024-03-30 11:19:45 +0530

    jenkins-file

--- 06-modules/sg/main.tf
+++ 06-modules/sg/main.tf
@@ -1,31 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-data "aws_ami" "centos8" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.centos8.id
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-
-  tags = {
-    Name = "test-centos8"
-  }
-}
-
-terraform {
-  backend "s3" {
-    bucket = "terraform-1230"
-    key    = "05-remote-state/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
@@ -61,4 +33,8 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
   security_group_id = aws_security_group.allow_tls.id
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+output "security_group_id" {
+  value = "aws_security_group.allow_tls.id"
 }
