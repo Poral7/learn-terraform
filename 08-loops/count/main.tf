0809de25641b6935965ef7fb4e2a247db5f97697
sunil <poralsunil5@gmail.com>
2024-03-31 07:19:50 +0530

    jenkins-file

--- 08-loops/count/main.tf
+++ 08-loops/count/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "web" {
-  count         = 1
+  count         = length(var.components)
   ami           = data.aws_ami.centos8.id
   instance_type = "t3.micro"
 
   tags = {
-    Name = "test-centos8"
+    Name = var.components[count.index]
   }
 }
 
@@ -20,4 +20,8 @@ output "public_ip" {
 
 #  with count
   value = aws_instance.web.*.public_ip
+}
+
+variable "components" {
+  default = ["cart","catalogue"]
 }
