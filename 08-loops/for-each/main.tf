4cfbdab2802d8962672bc7e98b0b17e155bfe90d
sunil <poralsunil5@gmail.com>
2024-03-31 21:53:39 +0530

    jenkins-file

--- 08-loops/for-each/main.tf
+++ 08-loops/for-each/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  fro_each      = var.components
+  for_each      = var.components
   ami           = data.aws_ami.centos8.id
   instance_type = each.value.instance_type
 
